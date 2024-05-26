edf4a09de8fa2bfb937ed2f07c55529e9b48480c
Anericokakai <challykiddoh@gmail.com>
2024-05-26 14:02:35 +0300

    fix

--- src/api/spring-boot/controllers/spring-boot.js
+++ src/api/spring-boot/controllers/spring-boot.js
@@ -6,4 +6,18 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::spring-boot.spring-boot');
+module.exports = createCoreController('api::spring-boot.spring-boot',({strapi})=>({
+    async findOne(ctx){
+        const {id}=ctx.params;
+        const entity= await strapi.db.query('api::spring-boot.spring-boot').findOne({
+            where:{
+                slug:id
+            }
+        })
+        const sanitizedEntity= await this.sanitizeOutput(entity,ctx);
+
+     return this.transformResponse(sanitizedEntity);
+    }
+}));
+
+
--- src/api/topic/controllers/topic.js
+++ src/api/topic/controllers/topic.js
@@ -6,4 +6,18 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::topic.topic');
+
+
+module.exports = createCoreController('api::topic.topic',({ strapi }) => ({
+    async findOne(ctx) {
+      const { id } = ctx.params;
+      const entity=await strapi.db.query('api::topic.topic').findOne({
+        where:{
+           slug:id 
+        }
+      });
+        const sanitizedEntity= await this.sanitizeOutput(entity,ctx);
+
+      return this.transformResponse(sanitizedEntity);
+    },
+  }));
