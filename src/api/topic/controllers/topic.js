7cb8bf9edf88b8e2337e35044bf99a0f6620ce29
Anericokakai <challykiddoh@gmail.com>
2024-11-10 10:26:25 +0300

    fix some

--- src/api/topic/controllers/topic.js
+++ src/api/topic/controllers/topic.js
@@ -1,23 +1,20 @@
-'use strict';
+"use strict";
 
 /**
  * topic controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
+module.exports = createCoreController("api::topic.topic", ({ strapi }) => ({
+  async findOne(ctx) {
+    const { id } = ctx.params;
+    const entity = await strapi.db.query("api::topic.topic").findOne({
+      where: {
+        slug: id,
+      },
+    });
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
 
-
-
-module.exports = createCoreController('api::topic.topic',({ strapi }) => ({
-    async findOne(ctx) {
-      const { id } = ctx.params;
-      const entity=await strapi.db.query('api::topic.topic').findOne({
-        where:{
-           slug:id 
-        }
-      });
-        const sanitizedEntity= await this.sanitizeOutput(entity,ctx);
-
-      return this.transformResponse(sanitizedEntity);
-    },
-  }));
+    return this.transformResponse(sanitizedEntity);
+  },
+}));
