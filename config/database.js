fe7094975ab4cb03796df3771e2baea8387dd36f
Anericokakai <challykiddoh@gmail.com>
2024-11-10 20:45:01 +0300

    fix

--- config/database.js
+++ config/database.js
@@ -33,7 +33,9 @@ module.exports = ({ env }) => {
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl:false
+        ssl: {
+          rejectUnauthorized: false,
+        },
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
